fd03a704aa68d3e198c8d396aee33b64b8b6fcc0
KonceptD <kiranjayd@yahoo.com>
2022-07-08 16:29:42 +0200

    Removed roundEngine function, currentRound variable; Moved playerSelection. playerScore and computerScore to be global variables; Eliminated a lot of redundant code in the game engine function; Debugged and now have a perfectly working game ;

--- javascript.js
+++ javascript.js
@@ -1,32 +1,46 @@
-    // assigned strings to variables
+   /* FINAL UPDATE: Took the long route around and added redundant code. Debugged and managed to reduce redundancy and got a perfectly working game!
+      I'm leaving my commented-out code here as reference for future reviewing down the line to see where I went wrong
+
+      Note: I must commit significant changes more, I got caught up in the rush and did it all one time, leaving a poor trail of what I did
+
+      KonceptD(KJ) - 08 July 2022 - 16:25 pm 
+   */
+   
+   // assigned strings to variables
     let rock = "rock"
     let paper = "paper"
     let scissors = "scissors"
-    let currentRound = 0;
+    // let currentRound = 0;
+    let playerSelection;
+    let playerScore;
+    let computerScore;
+    // let over;
 
 // Players choice and caters for incorrect input
 function playerChoice(){
     while (true) {
-        playerSelection = window.prompt("Rock, Paper or Scissors?: ").toLowerCase;
+        playerSelection = window.prompt("Rock, Paper or Scissors?: ").toLowerCase();
+         
         if (playerSelection == rock) {
             break;
-            return playerSelection;
+            
         }
         else if (playerSelection == paper) {
             break;
-            return playerSelection;
+            
         }
         else if (playerSelection == scissors) {
             break;
-            return playerSelection;
+            
         }
         else {
             window.prompt("Incorrect input. Try again")
             continue;
         }
         
-        }
-    }    
+    }
+return playerSelection;
+}    
 
 // initializes the computers random choice
 function computerPlay() {
@@ -40,132 +54,140 @@ function computerPlay() {
 
 let computerSelection = computerPlay();    
 
-// to determine when the round ends
-function roundEngine () {
-    if (currentRound == rounds) {
-        let over = "over"
-        return over
-    } 
-    else {
-        let over = "ongoing"
-        return over 
-    }
-}
+// // to determine when the round ends
+// function roundEngine () {
+//     if (currentRound == rounds) {
+//         over = "over"
+//         return over
+//     } 
+//     else {
+//         let over = "ongoing"
+//         return over 
+//     }
+// }
 
 // determines the winner
 function gameEngine () {
 
-    let playerScore = 0;
-    let computerScore = 0;
+    // playerScore = 0;
+    // computerScore = 0;
     
-    while (true) {
+    // while (true) {
 
         
         if (playerSelection == rock && computerSelection == scissors) {
             playerScore++
             console.log("Player wins.\n" + "Player: " + playerScore + "\nComputer: " + computerScore)
-            currentRound++
-            roundEngine();
-            if (over == "over") {
-                console.log("Game Over")
-                break
-            }
-            else {
-                continue
-            }
+            
+            // currentRound++
+            // roundEngine();
+            // if (over == "over") {
+            //     console.log("Game Over")
+            //     break
+            // }
+            // else {
+            //     continue
+            // }
         }
         else if (playerSelection == paper && computerSelection == rock) {
             playerScore++
             console.log("Player wins.\n" + "Player: " + playerScore + "\nComputer: " + computerScore)
-            currentRound++
-            roundEngine();
-            if (over == "over") {
-                console.log("Game Over")
-                break
-            }
-            else {
-                continue
-            }
+            
+            // currentRound++
+            // roundEngine();
+            // if (over == "over") {
+            //     console.log("Game Over")
+            //     break
+            // }
+            // else {
+            //     continue
+            // }
         }
         else if (playerSelection == scissors && computerSelection == paper) {
             playerScore++
             console.log("Player wins.\n" + "Player: " + playerScore + "\nComputer: " + computerScore)
-            currentRound++
-            roundEngine();
-            if (over == "over") {
-                console.log("Game Over")
-                break
-            }
-            else {
-                continue
-            }
+            // currentRound++
+            // roundEngine();
+            // if (over == "over") {
+            //     console.log("Game Over")
+            //     break
+            // }
+            // else {
+            //     continue
+            // }
         }
         else if (computerSelection == rock && playerSelection == scissors) {
             computerScore++
             console.log("Computer wins.\n" + "Player: " + playerScore + "\nComputer: " + computerScore)
-            currentRound++
-            roundEngine();
-            if (over == "over") {
-                console.log("Game Over")
-                break
-            }
-            else {
-                continue
-            }
+            // currentRound++
+            // roundEngine();
+            // if (over == "over") {
+            //     console.log("Game Over")
+            //     break
+            // }
+            // else {
+            //     continue
+            // }
         }
         else if (computerSelection == paper && playerSelection == rock) {
             playerScore++
             console.log("Computer wins.\n" + "Player: " + playerScore + "\nComputer: " + computerScore)
-            currentRound++
-            roundEngine();
-            if (over == "over") {
-                console.log("Game Over")
-                break
-            }
-            else {
-                continue
-            }
+            // currentRound++
+            // roundEngine();
+            // if (over == "over") {
+            //     console.log("Game Over")
+            //     break
+            // }
+            // else {
+            //     continue
+            // }
         }
         else if (computerSelection == scissors && playerSelection == paper) {
             playerScore++
             console.log("Computer wins.\n" + "Player: " + playerScore + "\nComputer: " + computerScore)
-            currentRound++
-            roundEngine();
-            if (over == "over") {
-                console.log("Game Over")
-                break
-            }
-            else {
-                continue
-            }
+            // currentRound++
+            // // roundEngine();
+            // // if (over == "over") {
+            // //     console.log("Game Over")
+            // //     break
+            // // }
+            // // else {
+            // //     continue
+            // // }
         }
         else {
             console.log("Tie.\n" + "Player: " + playerScore + "\nComputer: " + computerScore)
-            currentRound++
-            roundEngine();
-            if (over == "over") {
-                console.log("Game Over")
-                break
-            }
-            else {
-                continue
-            }
+            // currentRound++
+            // roundEngine();
+            // if (over == "over") {
+            //     console.log("Game Over")
+            //     break
+            // }
+            // else {
+            //     continue
+            // }
         }
+        return;
     }
-    return
-}
+    
+// }
 
 // Plays the game in the round number prompted 
 function playRound(rounds) {
+
+    playerScore = 0;
+    computerScore = 0;
         
     for (let i = 0; i < rounds ; i++ ) {
         playerChoice(); 
-        computerSelection();
+        computerSelection
+        console.log("Round: " + (i+1))
         gameEngine();
+        
 
     }
 
-
+    return console.log("Game Over");
 }
 
 
